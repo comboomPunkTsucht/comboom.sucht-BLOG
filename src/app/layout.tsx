b1b1bf8d4985a56d7c0126498cadbd8f24b7c7c6
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-08-02 10:35:12 +0200

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";  // Ensure you are using client-side rendering
-
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Particles from '@/components/particles';
@@ -7,7 +5,59 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { FlagValues } from '@vercel/flags/react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import Head from './head';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "comboom.sucht | The Mgaming Group",
+  description: "web page of comboom.sucht",
+  keywords: "comboom, sucht, comboom.sucht, comboompunktsucht, mcpeaps_HD, mahd, blackdragon",
+  twitter: {
+    card: 'summary_large_image',
+    title: 'comboom.sucht | The Mgaming Group',
+    description: 'web page of comboom.sucht',
+    siteId: '1349452878545629186',
+    creator: '@ComboomS',
+    creatorId: '1349452878545629186',
+    images: [{
+      url: 'https://comboompunksucht.app/pictures/1024.png',
+      alt: 'Logo of comboom.sucht',
+      width: 1024,
+      height: 1024,
+    }],
+  },
+  openGraph: {
+    type: 'website',
+    title: 'comboom.sucht | The Mgaming Group',
+    description: 'web page of comboom.sucht',
+    url: 'https://comboompunksucht.app',
+    images: [{
+      url: 'https://comboompunksucht.app/pictures/1024.png',
+      alt: 'Logo of comboom.sucht',
+      width: 1024,
+      height: 1024,
+    }],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    noarchive: false,
+    noimageindex: false,
+    "max-snippet": -1,
+    "max-image-preview": "large",
+    "max-video-preview": -1,
+    googleBot: {
+      index: true,
+      follow: true,
+      nocache: false,
+      noarchive: false,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
 
 // No need to export metadata here as it's already imported
 
@@ -17,10 +67,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const values = { exampleFlag: true };
-
+    console.log("metadata:", metadata);
   return (
     <html lang="de" suppressHydrationWarning>
-      <Head />
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#010101" />
+      <meta name="start_url" content="https://comboompunksucht.app/" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#010101" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-title" content="comboom.sucht | The Mgaming Group" />
+      <meta name="application-name" content="comboom.sucht" />
+      <meta name="msapplication-TileColor" content="#010101" />
+      <meta charSet="UTF-8" />
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <link rel="sitemap" href="/sitemap" />
+      <meta name="keywords" content="comboom, sucht, comboom.sucht, comboompunktsucht, mcpeaps_HD, mahd, blackdragon" />
+      <meta name="format-detection" content="telephone=yes, date=no, email=yes, address=yes" />
+    </head>
       <body>
         <UserProvider>
           <ThemeProvider
