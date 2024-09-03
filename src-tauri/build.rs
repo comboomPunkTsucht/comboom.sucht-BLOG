8504d818eb6787006a40bd2cdc409edea2a84eff
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-09-03 08:07:12 +0200

    .

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,34 @@
+use std::fs;
+
 fn main() {
+    // Lese Cargo.toml zur Build-Zeit
+    let content = fs::read_to_string("Cargo.toml").expect("Failed to read Cargo.toml");
+
+    // Extrahiere die Versionsnummer
+    let version_line = content
+        .lines()
+        .find(|line| line.starts_with("version = "))
+        .expect("Version not found");
+
+    let version = version_line.split('=')
+        .nth(1)
+        .expect("No version found")
+        .trim()
+        .trim_matches('"');
+
+        // Extrahiere die Versionsnummer
+    let description_line = content
+        .lines()
+        .find(|line| line.starts_with("description = "))
+        .expect("Description not found");
+
+    let description = description_line.split('=')
+        .nth(1)
+        .expect("No Description found")
+        .trim()
+        .trim_matches('"');
+
+    println!("cargo:rerun-if-changed=Cargo.toml");
+    println!("cargo:version={}", version);
     tauri_build::build()
 }
