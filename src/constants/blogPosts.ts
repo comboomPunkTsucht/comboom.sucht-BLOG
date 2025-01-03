aaeab4af32fcf7d13c50b6533f257adeece3e63f
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-01-03 17:22:00 +0100

    refactor blog post paths to use alias imports

--- src/constants/blogPosts.ts
+++ src/constants/blogPosts.ts
@@ -1,6 +1,6 @@
 export const blogPosts = {
-    'hello-world': require('../../public/blog/posts/hello-world.md'),
-    'test': require('../../public/blog/posts/test.md'),
-    'test2': require('@/../public/blog/posts/test2.md'),
+    'hello-world': require('@public/blog/posts/hello-world.md'),
+    'test': require('@public/blog/posts/test.md'),
+    'test2': require('@public/blog/posts/test2.md'),
     // Add all your blog posts here
 };
